fca97a405e97ecd4d74ed695aa1b4187ecd46ebd
AndreaGodsey <noirmenace101@gmail.com>
2023-11-07 22:24:46 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# n320-320homework-two
+ Making the Nav Links Work/Contact Page
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '@/styles/Home.module.css';
+
+
+<nav className={styles.naviDecoration}>
+    <a className={styles.naviLinksDecoration} href="#">Home</a>
+    <a className={styles.naviLinksDecoration} href="#">About</a>
+    <a className={styles.naviLinksDecoration} href="#">Products</a>
+    <a className={styles.naviLinksDecoration} href="#">Contact</a>
+    </nav>
